2e84ad9241ce3d06642b4e943157bd538bbb4fd5
Brent Miller <brentwmiller93@gmail.com>
2020-09-15 23:05:22 -0400

    Adds function to easily register patterns

--- iab-patterns/iab-patterns.php
+++ iab-patterns/iab-patterns.php
@@ -25,19 +25,31 @@ function iab_patterns() {
     
   }
 
+  iab_register_pattern('example-pattern', 'Example Pattern', 'A demostration pattern to show the power of patterns');
+}
+
+add_action('init', 'iab_patterns');
+
+/**
+ * Register patterns
+ *
+ * @param String $id (required) A unique name for your pattern
+ * @param String $title (required) A human-readable title for the pattern.
+ * @param String $description A visually hidden text used to describe the pattern in the inserter. A description is optional but it is strongly encouraged when the title does not fully describe what the pattern does.
+ * @param Array $categories (optional) A list of pattern categories used to group block patterns. Block patterns can be shown on multiple categories.
+ */
+function iab_register_pattern($id, $title, $description = '', $categories = array('iab-patterns')) {
   if (function_exists('register_block_pattern')) {
 
     register_block_pattern (
-      'iab-patterns/example-pattern',
+      'iab-patterns/' . $id,
       array (
-        'categories'    => array('iab-patterns'),
-        'title'       => __('Example Pattern', 'iab-patterns'),
-        'description' => _x('A demostration pattern to show the power of patterns', 'iab-patterns'),
-        'content'     => file_get_contents( plugin_dir_path( __FILE__ ) . 'patterns/example-pattern.txt'),
+        'categories'    => $categories,
+        'title'       => __($title, 'iab-patterns'),
+        'description' => _x($description, 'iab-patterns'),
+        'content'     => file_get_contents( plugin_dir_path( __FILE__ ) . 'patterns/' . $id . '.txt'),
       )
     );
 
   }
 }
-
-add_action('init', 'iab_patterns');
